23a8cc3c594e11f99fe70d210f165e5c5ae680f4
Lupeeeeeeeeee <guadalupeecuy@gmail.com>
2026-02-13 00:15:23 -0500

    puerto

--- conexion.php
+++ conexion.php
@@ -1,9 +1,10 @@
 <?php
-$conexion = mysqli_connect("mainline.proxy.rlwy.net", "root", "VCIRjIlFVFADHCUlsSLFMYxbJgOfdsUK", "railway");
+$conexion = mysqli_connect("mainline.proxy.rlwy.net", "root", "VCIRjIlFVFADHCUlsSLFMYxbJgOfdsUK", "railway","34174");
 
 if (!$conexion) {
     die("Error de conexión");
 }
 ?>
+
 
 
