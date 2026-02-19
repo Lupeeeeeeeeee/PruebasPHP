706db92208a4a0a21bd3a111489060df19b70fc4
Lupeeeeeeeeee <guadalupeecuy@gmail.com>
2026-02-19 17:22:30 -0500

    Use prepared statements in eliminar.php
    Refactor eliminar.php to use prepared statements for deletion.

--- eliminar.php
+++ eliminar.php
@@ -1,9 +1,18 @@
 <?php
 include("conexion.php");
 
-$id = $_GET['id'];
+if (isset($_GET['id'])) {
 
-mysqli_query($conexion, "DELETE FROM usuarios WHERE id=$id");
+    $id = $_GET['id'];
+
+    $stmt = mysqli_prepare($conexion, 
+        "DELETE FROM usuarios WHERE id=?");
+
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+}
 
 header("Location: index.php");
+exit();
 ?>
