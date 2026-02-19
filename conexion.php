5ed1f55a4830262608ca6fc3f2a1df67d40d5e54
Lupeeeeeeeeee <guadalupeecuy@gmail.com>
2026-02-19 17:21:53 -0500

    Improve connection error handling in conexion.php
    Added error message to connection failure.

--- conexion.php
+++ conexion.php
@@ -1,10 +1,13 @@
 <?php
-$conexion = mysqli_connect("mainline.proxy.rlwy.net", "root", "VCIRjIlFVFADHCUlsSLFMYxbJgOfdsUK", "railway","34174");
+$conexion = mysqli_connect(
+    "mainline.proxy.rlwy.net",
+    "root",
+    "VCIRjIlFVFADHCUlsSLFMYxbJgOfdsUK",
+    "railway",
+    34174
+);
 
 if (!$conexion) {
-    die("Error de conexión");
+    die("Error de conexión: " . mysqli_connect_error());
 }
 ?>
-
-
-
